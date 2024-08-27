2177f50e339b54e417491ceec0a48f76789ba5e5
Marya25Bat <maria_batres@ymail.com>
2024-08-26 20:43:12 -0700

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,23 @@
+// How to Calculate your Carbon Footprint
+
+// 1.
+const myHouse = 10;
+// 2.
+const homeSize = 2;
+// 3.
+const myFood = 6;
+// 4.
+const waterConsumption = 3;
+// 5.
+const housePurchase = 4;
+// 6. 
+const wasteProduce = 40;
+// 7.
+const wasteRec = 12;
+// 8.
+const transPort = 12;
+// 9. 
+const cfpTotal = myHouse + homeSize + myFood + waterConsumption + housePurchase + wasteProduce + wasteRec + transPort; 
+// 10. Write JS to update the rendered html (index.html) with total
+const endTotal = document.querySelector ("h2");
+endTotal.textContent = "89";
