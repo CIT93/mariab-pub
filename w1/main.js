0432de4b2744ecea2f5716b0195d1bb940239c16
Marya25Bat <maria_batres@ymail.com>
2024-09-03 13:40:43 -0700

    week 33 attendance

---
+++ w1/main.js
@@ -0,0 +1,22 @@
+console.log ("Hello from inside the main.js file");
+
+//let myVar = "25";
+
+const myVar = Maria;
+const myVarType = typeof(myVar);
+console.log(myVarType + myVarType);
+console.log('myVarType + ${myVarType$}');
+
+
+
+function runNow () {
+    console.log('inside the funtion');
+    if(myVarType ===  "number") {
+        console.log('will 11 this one run?')
+    } else {
+        console.log('will 13 this one run')
+    }
+}
+
+runNow();
+runNow();
